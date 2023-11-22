26af7047b90d70da550d98324f0256f607c10fff
Fatlum Haliti <fatlum-haliti@outlook.com>
2023-11-22 14:36:05 +0100

    enabled cors

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const express = require('express')
 var fs = require('fs');
 const path = require('path')
 const multer = require('multer');
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
 var imagePath = path.join(__dirname, '/images/');
 let jsonPath = path.join(__dirname, '/data/articles.json');
 
+// Takes the file and checks if it's a png, jpg file and uploads it to images directory
 const storage = multer.diskStorage({
   destination: './images',
   filename: (req, file, cb) => {
@@ -23,11 +25,17 @@ const storage = multer.diskStorage({
   },
 });
 
+// Enable CORS
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 const upload = multer({ storage: storage });
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
-app.get('/files', getDirectoryContent, (req, res) => {
+app.get('/files', getDirectoryContent, cors(corsOptions), (req, res) => {
     const arr = []
     
     fs.readFile(jsonPath, 'utf8', function (err, data) {
@@ -47,6 +55,7 @@ app.get('/files', getDirectoryContent, (req, res) => {
 
 })
 
+// The request to make the upload of image is done here
 app.post('/upload', upload.single('image'), async (req, res) => {
   const imageUrl = req.file.path;
 
@@ -62,6 +71,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// get the names of the images
 function getDirectoryContent(req, res, next) {
     fs.readdir(imagePath , function (err, images) {
       if (err) { return next(err); }
